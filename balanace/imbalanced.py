063cda168d76318f256c82d968af971c6dec0ff1
RaVincentHuang <18573104293@163.com>
2024-06-08 17:04:30 +0800

    Change the impl of imbalanced

--- balanace/imbalanced.py
+++ balanace/imbalanced.py
@@ -9,18 +9,34 @@ class Imbalanced(object):
             else:
                 self.hashmap[item] = 1
         
+        self.equal_size = len(self.column) / len(set(self.column))
+        
         self.min_value, self.min_counter = min(self.hashmap.items(), key=lambda x: x[1])
         self.second_min_counter = min(filter(lambda x : x != self.min_counter, self.hashmap.values()))
+        self.max_value, self.max_counter = max(self.hashmap.items(), key=lambda x: x[1])
+        self.second_max_counter = max(filter(lambda x : x != self.max_counter, self.hashmap.values()))
         
     
-    def enum_check(self, tA, delta) -> bool:
+    def enum_check_min(self, tA, delta) -> bool:
         if tA == self.min_value:
             if self.second_min_counter - self.hashmap[tA] < delta:
                 return True
             return False
-        elif self.min_counter - self.hashmap[tA] < delta:
+        elif self.hashmap[tA] - self.min_counter < delta * self.equal_size:
             return True
         return False
+    
+    def enum_check_max(self, tA, delta) -> bool:
+        if tA == self.max_value:
+            if self.hashmap[tA] - self.second_max_counter < delta * self.equal_size:
+                return True
+            return False
+        elif self.max_counter - self.hashmap[tA] < delta:
+            return True
+        return False
+    
+    def enum_check(self, tA, delta) -> bool:
+        return self.enum_check_min(tA, delta) or self.enum_check_max(tA, delta)
 
 
 if __name__ == "__main__":
